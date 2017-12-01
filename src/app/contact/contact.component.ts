79005a0bbeb80d4bb4c419dd4f4435044b3244ad
Ninad Vadujkar <nv_vit@yahoo.in>
2017-12-01 16:47:53 +0530

    Reactive forms part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -22,10 +22,10 @@ export class ContactComponent implements OnInit {
 
   createForm(): void {
     this.feedbackForm = this._fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: [0, Validators.required],
+      email: ['', Validators.required],
       agree: false,
       contacttype: [{value: 'None', disabled: true}],
       message: ''
@@ -34,16 +34,20 @@ export class ContactComponent implements OnInit {
 
   onSubmit(): void {
     this.feedback = this.feedbackForm.value;
-    this.feedbackForm.reset();
     console.log(this.feedback);
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
   }
 
   onToggle(e): void {
-    if(e.checked) {
-      this.feedbackForm.controls['contacttype'].enable();
-    } else {
-      this.feedbackForm.controls['contacttype'].disable();
-    }
+    e.checked ? this.feedbackForm.controls['contacttype'].enable() : this.feedbackForm.controls['contacttype'].disable();
   }
   
 
