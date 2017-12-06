d9ec8c3bff56d26137c2dcd90e156392d3f34626
Ninad Vadujkar <nv_vit@yahoo.in>
2017-12-06 13:57:52 +0530

    e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('coursera-angular4-course App', () => {
   let page: AppPage;
@@ -7,8 +8,32 @@ describe('coursera-angular4-course App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
+  });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+  
+    let navLink = page.getAllElements('a').get(1);
+    navLink.click();
+
+    expect(page.getParagraphText('h3')).toEqual('About Us');
+  });
+
+  it('should enter a new comment for first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    let newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    let newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    let newSubmitBtn = page.getElement('button[type=submit]');
+    newSubmitBtn.click();
+
+    browser.pause();
   });
 });
